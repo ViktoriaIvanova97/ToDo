12485f5e51145c74fa640304668ae7764f233041
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-10-16 22:23:07 +0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter basename="/ToDo/">
       <App />
-    </BrowserRouter>
   </Provider>
 );
