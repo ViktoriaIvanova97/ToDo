73a03879283880e8dbc2abceb9d86f66450183dd
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-10-21 17:13:16 +0300

    add store RTK

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,25 @@
-import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
-import { tasksReducer } from "./reducers/tasksReducer";
-import { localStorageMiddleware } from "./localStorageMiddleware";
+import { tasksReducer } from './reducers/tasksReducer'
+import { localStorageMiddleware } from './localStorageMiddleware'
+import { configureStore } from '@reduxjs/toolkit'
 
 const loadFromLocalStorage = () => {
   try {
-    const state = localStorage.getItem("tasksState");
-    if (!state) return undefined;
-    return { tasks: JSON.parse(state) };
+    const state = localStorage.getItem('tasksState')
+    if (!state) return undefined
+    return { tasks: JSON.parse(state) }
   } catch (e) {
-    console.error("Ошибка при загрузке из localStorage:", e);
-    return undefined;
+    console.error('Ошибка при загрузке из localStorage:', e)
+    return undefined
   }
-};
+}
 
-const persistedState = loadFromLocalStorage();
+const persistedState = loadFromLocalStorage()
 
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-});
-
-export const store = legacy_createStore(
-  rootReducer,
-  persistedState,
-  applyMiddleware(localStorageMiddleware),
-);
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+  },
+  preloadedState: persistedState,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
+})
