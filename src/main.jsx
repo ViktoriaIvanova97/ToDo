0660887d8e835839e853b88252fcd1e56641f647
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-10-31 19:02:24 +0300

    add HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom"
 import { store } from './redux/store'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter basename='/ToDo'>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
